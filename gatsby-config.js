12c597a1865a544a008f85657990e6d0c57d54fa
jginesclavero <jginesclavero@gmail.com>
2020-07-08 20:12:10 +0200

    Adding prefix-path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: '/actros.github.io',
   siteMetadata: {
     title: `nehalem-starter`,
     siteUrl: `https://nehalem.netlify.com`,
@@ -52,7 +53,7 @@ module.exports = {
         manifest: {
           name: `nehalem - A Gatsby theme`,
           short_name: `nehalem`,
-          start_url: `/actros.github.io`,
+          start_url: `/`,
           background_color: `#a4cbb8`,
           theme_color: `#a4cbb8`,
           display: `minimal-ui`,
