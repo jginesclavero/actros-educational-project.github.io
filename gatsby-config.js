6006fd38b3acbda9c6cdec624a97fc1f7316d250
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-07-08 21:05:02 +0200

    [gatsby-config.js]url name changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `nehalem-starter`,
-    siteUrl: `https://nehalem.netlify.com`,
+    siteUrl: `https://actros-educational-project.github.io`,
     description: `A Gatsby starter for nehalem`,
     topics: [],
     menu: [
