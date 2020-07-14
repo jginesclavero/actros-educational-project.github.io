70c0705bec14166042c69ba7131439e042e43598
jginesclavero <jginesclavero@gmail.com>
2020-07-14 11:18:32 +0200

    style header background image

--- src/@nehalist/gatsby-theme-nehalem/styles/theme.ts
+++ src/@nehalist/gatsby-theme-nehalem/styles/theme.ts
@@ -26,7 +26,7 @@ const Theme: DefaultTheme = {
     header: {
       height: `370px`,
       //background: `linear-gradient(-45deg, #761e6d, #e61a0c) repeat scroll 0 0 transparent`,
-      background: `#ffffff url(${ backgroundUrl }) 50% 50% no-repeat;`,
+      background: `#ffffff url(${ backgroundUrl }) 50% 40% 20% no-repeat; fixed`,
     },
   },
 };
