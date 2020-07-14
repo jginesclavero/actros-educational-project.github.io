91498d9304508e11dd754967d013fc515a61d793
jginesclavero <jginesclavero@gmail.com>
2020-07-14 11:33:55 +0200

    rm icon and new header topics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `ATCROS`,
+    title: `ATCROS1`,
     siteUrl: `https://actros-educational-project.github.io`,
     description: `Center for Advanced Training on Robotics and %TOPICS%`,
     topics: [
-      'Open Source'
+      'Open Source',
+      'Navigation',
+      'Deep Learning',
+      'Perception',
     ],
     menu: [
       {
@@ -34,7 +37,7 @@ module.exports = {
     ],
     search: true,
     author: {
-      name: `ATCROS`,
+      name: `ATCROS2`,
       description: `The project <a href="https://www.rosin-project.eu/ftp/center-for-advanced-training-on-robotics-and-open-source-act-ros-education-project" target="_blank"><strong>“Center for Advanced Training on Robotics and Open Source (ACTROS)”</strong></a> aims to create a reference center in mobile robotics training using Open Source, mainly ROS and ROS2. This center is within the <a href="https://www.urjc.es/" target="_blank"><strong>Rey Juan Carlos University</strong></a>, which is currently a national benchmark in ROS training, both by university courses taught since 2015 (Advanced University Course in Robot Programming with ROS) and by the creation of a Degree in Software Robotics, which is a pioneer in our country because of its focus on software development for robots.
       This project aims to equip the existing laboratory robotics robots reference in the field (Pepper, Tiago, Kobuki, etc.) to carry out regularly run courses both for advanced students, and professionals who want to train in software development for robots.`,
       social: {
@@ -59,7 +62,7 @@ module.exports = {
           background_color: `#a4cbb8`,
           theme_color: `#a4cbb8`,
           display: `minimal-ui`,
-          icon: `${__dirname}/content/assets/images/intellrobotslab_icon.png`
+          icon: ``
         }
       }
     }
