de2a6cd902cec3d993dde785bf3c567731261e81
jginesclavero <jginesclavero@gmail.com>
2020-07-21 12:01:32 +0200

    moving header text

--- src/@nehalist/gatsby-theme-nehalem/components/header/style.tsx
+++ src/@nehalist/gatsby-theme-nehalem/components/header/style.tsx
@@ -18,7 +18,7 @@ export const TitleWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 102%;
+  height: 104%;
   flex-direction: column;
 
   @media (max-width: ${Theme.breakpoints.sm}) {
